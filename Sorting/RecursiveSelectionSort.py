7bfc520ddbf4cca72a2e6eade8375d0198c891f3
sonivaibhav27 <52574535+sonivaibhav27@users.noreply.github.com>
2020-10-28 16:06:31 +0530

    Update RecursiveSelectionSort.py

--- Sorting/RecursiveSelectionSort.py
+++ Sorting/RecursiveSelectionSort.py
@@ -7,11 +7,27 @@ class SelectionSort:
         for i in range(start,len(self.arr)):
             if (self.arr[start]  >self.arr[i]):
                 self.arr[start],self.arr[i]= self.arr[i],self.arr[start]
-            print(str(i)+"->" + str(self.arr))
         self.sort(start+1)
+    def sortIterative(self,reverse=False):
+        if (reverse):
+            for i in range(len(self.arr)):
+                max = i
+                for j in range(i+1,len(self.arr)):
+                    if(self.arr[max] < self.arr[j]):
+                        max =j
+                self.arr[max],self.arr[i] = self.arr[i],self.arr[max]
+        else:  
+            for i in range(len(self.arr)):
+                min = i
+                for j in range(i+1,len(self.arr)):
+                    if(self.arr[min] > self.arr[j]):
+                        min =j
+                self.arr[min],self.arr[i] = self.arr[i],self.arr[min]
+                
     def print(self):
         print(self.arr)
 arr= SelectionSort([23,54,2,43,10,8,18])
-arr.sort()
+# arr.sort()
+arr.sortIterative(True)
 arr.print()
                 
